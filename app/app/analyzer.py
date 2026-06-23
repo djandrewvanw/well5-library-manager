22f7bf7c2f5cfff068bc1e9faefcfbbd5fe00847
djandrewvanw <djandrewvanw@gmail.com>
2026-06-23 17:41:55 +0200

    Create analyzer.py
    Dodano analizę BPM

---
+++ app/app/analyzer.py
@@ -0,0 +1,33 @@
+import librosa
+
+
+def analyze_track(file):
+
+    try:
+
+        y, sr = librosa.load(
+            file,
+            duration=60
+        )
+
+        tempo, beats = librosa.beat.beat_track(
+            y=y,
+            sr=sr
+        )
+
+        bpm = round(
+            float(tempo)
+        )
+
+        return {
+            "bpm": bpm,
+            "key": "Unknown"
+        }
+
+
+    except Exception:
+
+        return {
+            "bpm": 0,
+            "key": "Unknown"
+        }
