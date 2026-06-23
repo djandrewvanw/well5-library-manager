d33ab80c4dffb4dc18a477ca3885ebcc4083de5e
djandrewvanw <djandrewvanw@gmail.com>
2026-06-23 17:27:41 +0200

    Create dj_tags.py
    Dodano system MyTag DJ

---
+++ app/app/app/app/app/app/dj_tags.py
@@ -0,0 +1,35 @@
+DJ_COLORS = [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Purple",
+    "White"
+]
+
+
+EVENT_TYPES = [
+    "Club",
+    "Festival",
+    "Wedding",
+    "Beach",
+    "Radio"
+]
+
+
+ENERGY_LEVELS = [
+    "Low",
+    "Medium",
+    "High",
+    "Peak"
+]
+
+
+MOODS = [
+    "Dark",
+    "Happy",
+    "Sexy",
+    "Emotional",
+    "Aggressive"
+]
