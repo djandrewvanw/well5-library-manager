d12382aac92488e937226e7860912f1e5e58fd13
djandrewvanw <djandrewvanw@gmail.com>
2026-06-23 17:49:17 +0200

    Update tags.py
    Dodano zapis MyTag do MP3

--- app/app/app/app/tags.py
+++ app/app/app/app/tags.py
@@ -1,22 +1,72 @@
-from mutagen.id3 import ID3, POPM, APIC
+from mutagen.id3 import ID3, POPM, APIC, TXXX
+
+
+def get_id3(file):
+
+    try:
+        return ID3(file)
+
+    except:
+
+        return ID3()
+
 
 
 def save_rating(file, rating):
 
-    try:
-
-        audio = ID3(file)
-
-    except:
-
-        audio = ID3()
-
+    audio = get_id3(file)
 
     audio.add(
         POPM(
             email="Well5Library",
             rating=rating * 51,
             count=0
+        )
+    )
+
+    audio.save()
+
+
+
+def save_dj_tags(
+    file,
+    color,
+    event,
+    energy,
+    mood
+):
+
+    audio = get_id3(file)
+
+
+    audio.add(
+        TXXX(
+            description="DJ Color",
+            text=color
+        )
+    )
+
+
+    audio.add(
+        TXXX(
+            description="Event Type",
+            text=event
+        )
+    )
+
+
+    audio.add(
+        TXXX(
+            description="Energy",
+            text=energy
+        )
+    )
+
+
+    audio.add(
+        TXXX(
+            description="Mood",
+            text=mood
         )
     )
 
@@ -27,13 +77,7 @@ def save_rating(file, rating):
 
 def save_cover(file, image):
 
-    try:
-
-        audio = ID3(file)
-
-    except:
-
-        audio = ID3()
+    audio = get_id3(file)
 
 
     with open(image, "rb") as img:
